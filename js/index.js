5bd2c95cc487038f593832c0b229bb9142a0a7d1
karan147369 <103363950+karan147369@users.noreply.github.com>
2023-08-17 23:27:12 +0530

    bug fixed

--- js/index.js
+++ js/index.js
@@ -1,37 +1,39 @@
-const addToWatchlist = () => { }
 (async function () {
+  let response = await fetch("https://omdbapi.com/?apikey=2f2e0ecc&s=2023", {
+    method: "GET",
+  });
+  let data = await response.json();
+  const element = document.getElementById("container");
 
-    let response = await fetch('https://omdbapi.com/?apikey=2f2e0ecc&s=2023', {
-        method: 'GET'
-    })
-    let data = await response.json();
-    const element = document.getElementById('container');
-
-    let str = ``;
-    for (let i of data.Search) {
-
-        str += `<a href='#'><div class='child' id='${i.imdbID}'>
+  let str = ``;
+  for (let i of data.Search) {
+    str += `<a href='#'><div class='child' id='${i.imdbID}'>
         <img src=${i.Poster} height='200px' width='200px'></img>
-        <div>${(i.Title).substring(0, 40)}</div>
+        <div>${i.Title.substring(0, 40)}</div>
         <div>${i.Year}</div>
         </div></a>`;
-    }
-    str += ``;
-    element.innerHTML = str;
-    const array = document.getElementsByClassName('child');
-    Array.from(array).forEach((i) => {
-        i.addEventListener('click', async () => {
-            const watchlist = JSON.parse(localStorage.getItem('list'))
-            let alreadyAddedToWatchlist = false;
-            for (let j of watchlist.list) {
-                console.log(j + " " + i.id)
-                if (i.id == j) alreadyAddedToWatchlist = true;
-            }
-            const data = await fetch(`https://omdbapi.com/?apikey=2f2e0ecc&i=${i.id}`);
-            const movieDetail = await data.json();
-            // console.log(movieDetail)
-            if (!alreadyAddedToWatchlist) {
-                element.innerHTML = `
+  }
+  str += ``;
+  element.innerHTML = str;
+  const array = document.getElementsByClassName("child");
+  Array.from(array).forEach((i) => {
+    i.addEventListener("click", async () => {
+      let watchlist = JSON.parse(localStorage.getItem("list"));
+      let alreadyAddedToWatchlist = false;
+      if (watchlist == null) {
+        localStorage.setItem("list", JSON.stringify({ list: [] }));
+      }
+      watchlist = JSON.parse(localStorage.getItem("list"));
+      for (let j of watchlist.list) {
+        if (i.id == j) alreadyAddedToWatchlist = true;
+      }
+      const data = await fetch(
+        `https://omdbapi.com/?apikey=2f2e0ecc&i=${i.id}`
+      );
+      const movieDetail = await data.json();
+      // console.log(movieDetail)
+      if (!alreadyAddedToWatchlist) {
+        element.innerHTML = `
             <div id='poster'><img src=${movieDetail.Poster} alt='Poster Not Available'></img></div>
             <div id='movieDescription'>
             <p>${movieDetail.Title}</p>
@@ -40,10 +42,9 @@ const addToWatchlist = () => { }
             <p>${movieDetail.Genre}</p>
             <button id='addtowatchlist'>Add to watchlist</button>
             </div >
-             `
-            }
-            else {
-                element.innerHTML = `
+             `;
+      } else {
+        element.innerHTML = `
             <div id='poster'><img src=${movieDetail.Poster} alt='Poster Not Available'></img></div>
             <div id='movieDescription'>
             <p>${movieDetail.Title}</p>
@@ -52,51 +53,43 @@ const addToWatchlist = () => { }
             <p>${movieDetail.Genre}</p>
             <button id='' disabled='true'>Already added to watchlist</button>
             </div >
-             `
-            }
+             `;
+      }
 
-            document.getElementById('addtowatchlist').addEventListener('click', () => {
+      document
+        .getElementById("addtowatchlist")
+        .addEventListener("click", () => {
+          let obj = JSON.parse(localStorage.getItem("list"));
+          if (obj !== null)
+            localStorage.setItem(
+              "list",
+              JSON.stringify({ list: [...obj.list, i.id] })
+            );
+          else localStorage.setItem("list", JSON.stringify({ list: [i.id] }));
+        });
+    });
+  });
 
-                let obj = JSON.parse(localStorage.getItem('list'));
-                if (obj !== null) localStorage.setItem('list', JSON.stringify({ list: [...obj.list, i.id] }));
-                else localStorage.setItem('list', JSON.stringify({ list: [i.id] }));
-            })
-
-
-        })
-    })
-
-    const search = document.getElementsByTagName('input')[0];
-    search.addEventListener('change', async () => {
-        const url = `https://omdbapi.com/?apikey=2f2e0ecc&s=${search.value}`;
-        const searchResult = await fetch(url, { method: 'GET' });
-        const data = await searchResult.json();
-        const container = document.getElementById('container');
-        console.log(data)
-        let str = ``;
-        if (data.Response === 'False') {
-            str = `<div id='message'>No Movie found<div>`;
-        }
-        else {
-
-            for (let i of data.Search) {
-                str += `<a href=''><div class='child' id='${i.imdbID}'>
+  const search = document.getElementsByTagName("input")[0];
+  search.addEventListener("change", async () => {
+    const url = `https://omdbapi.com/?apikey=2f2e0ecc&s=${search.value}`;
+    const searchResult = await fetch(url, { method: "GET" });
+    const data = await searchResult.json();
+    const container = document.getElementById("container");
+    console.log(data);
+    let str = ``;
+    if (data.Response === "False") {
+      str = `<div id='message'>No Movie found<div>`;
+    } else {
+      for (let i of data.Search) {
+        str += `<a href=''><div class='child' id='${i.imdbID}'>
             <img src=${i.Poster} height='200px' width='200px'>
-            <div>${(i.Title).substring(0, 40)}</div>
+            <div>${i.Title.substring(0, 40)}</div>
             <div>${i.Year}</div>
             </div>`;
-            }
-            str += ``;
-
-        }
-        container.innerHTML = str;
-
-
-    });
-
-})()
-
-
-
-
-
+      }
+      str += ``;
+    }
+    container.innerHTML = str;
+  });
+})();
